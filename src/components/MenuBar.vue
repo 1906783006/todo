2878e55f94e680e0f45ca336505b2369f78f3cd7
unknown <1906783006@qq.com>
2020-08-30 00:04:37 +0800

    增加菜单栏组件

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="nav">
+    <div class="btn">
+      <a>新建</a>
+      <a>全部</a>
+      <a>3</a>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import { component } from 'vue/types/umd';
+
+@Component
+export default class MenuBar extends Vue {
+
+}
+</script>
+
+<style lang="less">
+  .nav {
+    background-color: pink;
+    padding: 0 20px;
+    overflow: hidden;
+    .btn {
+      float: right;
+      a {
+        padding: 6px;
+        cursor: pointer;
+        &:hover {
+          color: #fefefe;
+        }
+      }
+    }
+  }
+</style>
